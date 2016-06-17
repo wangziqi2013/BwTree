3478ecb4719c4503308abae3da91982709a4e521
Ziqi Wang <675973885@qq.com>
2016-06-17 20:00:21 +0000

    Adding STL test

---
+++ stl_test/vector_capacity.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<int> v{};
+	cout << "Initial Capacity: " << v.capacity() << endl;
+	for(int i = 0;i < 100;i++) {
+		v.push_back(i);
+		cout << "Capacity: " << v.capacity() << endl;
+	}
+
+	return 0;
+}
