4c7ebc366803ae77053f01de4fc7a3312af113ce
= <675973885@qq.com>
2016-10-27 19:04:39 -0400

    Change the order btree benchmarks are called

--- test/main.cpp
+++ test/main.cpp
@@ -121,10 +121,11 @@ int main(int argc, char **argv) {
     uint64_t thread_num = GetThreadNum();
     
     BenchmarkBTreeSeqInsert(t, key_num, (int)thread_num);
+    // Let this go before any of the other
+    BenchmarkBTreeZipfLockLessRead(t, key_num, (int)thread_num);
     BenchmarkBTreeSeqRead(t, key_num, (int)thread_num);
     BenchmarkBTreeRandRead(t, key_num, (int)thread_num);
     BenchmarkBTreeZipfRead(t, key_num, (int)thread_num);
-    BenchmarkBTreeZipfLockLessRead(t, key_num, (int)thread_num);
     
     DestroyBTree(t);
   }
