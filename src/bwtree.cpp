5a6f7b9c4df56af2287c3c0df77ef8bd74c6f36e
= <675973885@qq.com>
2016-11-23 22:46:00 -0500

    Adding initialization for thread_local variable

--- src/bwtree.cpp
+++ src/bwtree.cpp
@@ -23,6 +23,11 @@ namespace bwtree {
 
 bool print_flag = true;
 
+// This will be initialized when thread is initialized and in a per-thread
+// basis, i.e. each thread will get the same initialization image and then
+// is free to change them
+thread_local int BwTreeBase::gc_id = -1;
+
 }  // End index/bwtree namespace
 }  // End peloton/wangziqi2013 namespace
 
