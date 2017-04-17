25d1b2daf46abe214f6fc5a33773f99c2f2bd709
Ziqi Wang <675973885@qq.com>
2017-04-16 21:22:55 -0400

    Adding class GarbageGroup

--- src/thread_local.h
+++ src/thread_local.h
@@ -23,7 +23,70 @@ class GarbageNode {
 
   // Opaque pointer
   void *ptr;
+  
+  /*
+   * Constructor
+   */
+  GarbageNode(EpochType p_delete_epoch, void *p_ptr) :
+    delete_epoch{p_delete_epoch},
+    ptr{p_ptr}
+  {}
+  
+  /*
+   * Constructor
+   */
+  GarbageNode() {}
+  
+  // We do not allow any form of copy construcion and assignment
+  GarbageNode(const GarbageNode &) = delete;
+  GarbageNode(GarbageNode &&) = delete;
+  GarbageNode &operator=(const GarbageNode &) = delete;
+  GarbageNode &operator=(GarbageNode &&) = delete;
 };
+
+/*
+ * class GarbageGroup - A group of garbage nodes that are maintained in batch
+ *
+ * Also garbage nodes are allocated using slab allocator here, which reduces
+ * the number of allocator calls when we add and free garbages
+ *
+ * The garbage group instance is only processed if it is full and all nodes
+ * inside of it could be freed. To achieve this everytime we add a new node
+ * into the group we update the epoch counter also inside the object.
+ */
+class GarbageGroup {
+ public:
+  // There are 1024 slots in each garbage group before it is full
+  static constexpr GROUP_SIZE = 1024;
+  
+  // Number of nodes in this object
+  // We use this as a next index into the array
+  // and also use it to access the last deleted epoch
+  int node_count;
+  
+  // This is the next group object in the linked list
+  GarbageGroup *next_p;
+  
+  // This is the static array of garbage nodes. We only push
+  // the latest garbage node into this list
+  GarbageNode garbage_node_list[GROUP_SIZE];
+  
+  /*
+   * Constructor
+   */
+  GarbageGroup() :
+    node_count{0},
+    next_p{nullptr},
+    garnage_node_list{}
+  {}
+  
+  // Do not allow any othre form of assignmemt and construction
+  GarbageGroup(const GarbageGroup &) = delete;
+  GarbageGroup(GarbageGroup &&) = delete;
+  GarbageGroup &operator=(const GarbageGroup &) = delete;
+  GarbageGroup &operator=(GarbageGroup &&) = delete;
+};
+
 
 /*
  * class BwTreeThreadLocal - The thread local class that holds per-thread
