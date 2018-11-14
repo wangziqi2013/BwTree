09b7354d419513ac5648980e875e1a3246be396a
Ziqi Wang <ziqiw@mit.edu>
2018-11-14 06:55:21 -0500

    Fixing BF test

--- stl_test/bloom_filter.cpp
+++ stl_test/bloom_filter.cpp
@@ -8,11 +8,12 @@ using namespace std;
 
 int main() {
   const int *buffer[256];
-  
+  int temp[256]; 
   BloomFilter<int> bf{buffer};
   
   for(int i = 0;i < 256;i++) {
-    bf.Insert(i);
+    temp[i] = i;
+    bf.Insert(temp[i]);
   }
   
   cout << "Exists() result: ";
